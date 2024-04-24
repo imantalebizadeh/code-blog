68fe7cde555624b7e59b7f8e234be7760b75cba7
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-04-24 23:46:33 +0330

    feat: Create a constant for navigation links

---
+++ src/lib/constants.ts
@@ -0,0 +1,16 @@
+import { NavItem } from "@/types";
+
+export const navLinks: NavItem[] = [
+  {
+    title: "همه مقالات",
+    href: "/posts",
+  },
+  {
+    title: "درباره ما",
+    href: "/about-us",
+  },
+  {
+    title: "قوانین و مقرارت",
+    href: "/terms-conditions",
+  },
+];
