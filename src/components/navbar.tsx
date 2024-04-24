432df09cf754d28f2b12627ea22aa6f6a5ba872f
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-04-24 23:44:07 +0330

    feat: Add the mobile menu to the navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,28 @@
 import dynamic from "next/dynamic";
 import Link from "next/link";
 
-import CustomTooltip from "./common/custom-tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
 import Icon from "./common/icon";
 import Logo from "./logo";
+import MobileNav from "./mobile-nav";
 import NavLinks from "./nav-links";
-import ThemeToggle from "./theme-toggle";
 import { Button } from "./ui/button";
+
+const ThemeToggle = dynamic(() => import("@/components/theme-toggle"), {
+  ssr: false,
+});
 
 export default async function Navbar() {
   return (
     <header className="container flex items-center justify-between py-6">
       {/* Mobile menu button */}
-      {/* <MobileNav /> */}
+      <MobileNav />
 
       {/* Logo and desktop navigation links */}
       <div className="flex items-center gap-10">
@@ -25,13 +35,24 @@ export default async function Navbar() {
 
       {/* Theme and login buttons */}
       <div className="flex gap-3">
-        <CustomTooltip label={"ورود به حساب کاربری"} side="right">
-          <Button className="rounded-full" size={"icon"} variant={"secondary"}>
-            <Link href={"/login"}>
-              <Icon name={"UserRound"} size={20} />
-            </Link>
-          </Button>
-        </CustomTooltip>
+        <TooltipProvider>
+          <Tooltip delayDuration={300}>
+            <TooltipTrigger asChild>
+              <Button
+                className="rounded-full"
+                size={"icon"}
+                variant={"secondary"}
+              >
+                <Link href={"/login"}>
+                  <Icon name={"UserRound"} size={20} />
+                </Link>
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent side={"right"}>
+              <p>ورود به حساب کاربری</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
 
         <ThemeToggle className="hidden md:inline-flex" />
       </div>
