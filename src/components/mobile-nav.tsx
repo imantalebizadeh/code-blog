740636bdb15316193bb83f40c11d888f6f32db2c
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-04-24 23:43:17 +0330

    feat: Create a navbar for mobile mode

---
+++ src/components/mobile-nav.tsx
@@ -0,0 +1,32 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import Icon from "./common/icon";
+import NavLinks from "./nav-links";
+import { Button } from "./ui/button";
+
+export default function MobileNav() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button
+          type="button"
+          className="inline-flex rounded-full md:hidden"
+          size={"icon"}
+          variant={"secondary"}
+        >
+          <Icon name="Menu" size={20} />
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader />
+
+        <NavLinks className="mt-5" />
+      </SheetContent>
+    </Sheet>
+  );
+}
