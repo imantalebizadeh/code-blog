8aef9270189534983486ca1becbb801d88e3820c
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-04-27 12:22:52 +0330

    feat: Improve validations

--- src/lib/validations/auth.ts
+++ src/lib/validations/auth.ts
@@ -1,15 +1,25 @@
 import { z } from "zod";
 
 export const signInSchema = z.object({
-  email: z.string().email("لطفا یک ایمیل معتبر وارد کنید"),
-  password: z.string().min(1, "لطفا رمز عبور خود را وارد کنید"),
+  email: z
+    .string({ required_error: "وارد کردن ایمیل الزامی می باشد" })
+    .min(1, "وارد کردن ایمیل الزامی می باشد")
+    .email("لطفا یک ایمیل معتبر وارد کنید"),
+  password: z
+    .string({ required_error: "لطفا رمز عبور خود را وارد کنید" })
+    .min(1, "لطفا رمز عبور خود را وارد کنید"),
 });
 
 export const signUpSchema = z.object({
   name: z
-    .string()
+    .string({ required_error: "وارد کردن نام و نام خانوادگی الزامی می باشد" })
     .min(3, "نام و نام خانوادگی حداقل باید 3 کاراکتر باشد")
     .max(50, "نام و نام خانوادگی حداکثر باید 50 کاراکتر باشد"),
-  email: z.string().email("لطفا یک ایمیل معتبر وارد کنید"),
-  password: z.string().min(6, "رمز عبور حداقل باید 6 کاراکتر باشد"),
+  email: z
+    .string({ required_error: "وارد کردن ایمیل الزامی می باشد" })
+    .min(1, "وارد کردن ایمیل الزامی می باشد")
+    .email("لطفا یک ایمیل معتبر وارد کنید"),
+  password: z
+    .string({ required_error: "لطفا رمز عبور خود را وارد کنید" })
+    .min(6, "رمز عبور حداقل باید 6 کاراکتر باشد"),
 });
