afc4ca9bca48af67b77f7658cde48451137e91b9
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-04-27 12:20:11 +0330

    feat: Display the user's menu in the navbar after login

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,8 @@
 import dynamic from "next/dynamic";
 import Link from "next/link";
 
+import { auth } from "@/server/auth";
+
 import {
   Tooltip,
   TooltipContent,
@@ -13,12 +15,15 @@ import Logo from "./logo";
 import MobileNav from "./mobile-nav";
 import NavLinks from "./nav-links";
 import { Button } from "./ui/button";
+import UserMenu from "./user-menu";
 
 const ThemeToggle = dynamic(() => import("@/components/theme-toggle"), {
   ssr: false,
 });
 
 export default async function Navbar() {
+  const session = await auth();
+
   return (
     <header className="container flex items-center justify-between py-6">
       {/* Mobile menu button */}
@@ -35,24 +40,28 @@ export default async function Navbar() {
 
       {/* Theme and login buttons */}
       <div className="flex gap-3">
-        <TooltipProvider>
-          <Tooltip delayDuration={300}>
-            <TooltipTrigger asChild>
-              <Button
-                className="rounded-full"
-                size={"icon"}
-                variant={"secondary"}
-              >
-                <Link href={"/sign-in"}>
-                  <Icon name={"UserRound"} size={20} />
-                </Link>
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent side={"right"}>
-              <p>ورود به حساب کاربری</p>
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+        {session?.user ? (
+          <UserMenu user={session.user} />
+        ) : (
+          <TooltipProvider>
+            <Tooltip delayDuration={300}>
+              <TooltipTrigger asChild>
+                <Button
+                  className="rounded-full"
+                  size={"icon"}
+                  variant={"secondary"}
+                >
+                  <Link href={"/login"}>
+                    <Icon name={"UserRound"} size={20} />
+                  </Link>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side={"right"}>
+                <p>ورود به حساب کاربری</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        )}
 
         <ThemeToggle className="hidden md:inline-flex" />
       </div>
