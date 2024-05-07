d6872f20714c3101869f666ed7ed30f0a9475976
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-05-07 22:51:33 +0330

    feat: Create an authenticated instance of the action client - handle server errors

--- src/lib/action.client.ts
+++ src/lib/action.client.ts
@@ -1,3 +1,34 @@
 import { createSafeActionClient } from "next-safe-action";
 
-export const action = createSafeActionClient();
+import { auth } from "@/server/auth";
+
+import { DEFAULT_SERVER_ERROR } from "@/lib/constants";
+
+export const action = createSafeActionClient({
+  handleReturnedServerError(e) {
+    if (e instanceof Error) {
+      return e.message;
+    }
+
+    return DEFAULT_SERVER_ERROR;
+  },
+});
+
+export const authAction = createSafeActionClient({
+  handleReturnedServerError(e) {
+    if (e instanceof Error) {
+      return e.message;
+    }
+
+    return DEFAULT_SERVER_ERROR;
+  },
+  async middleware() {
+    const session = await auth();
+
+    if (!session || !session.user) {
+      throw new Error("احراز هویت صورت نگرفته است");
+    }
+
+    return session.user;
+  },
+});
