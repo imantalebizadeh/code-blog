9b339ea10b29ca1c92ba71ecdd737060a6a521b8
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-05-07 22:54:58 +0330

    feat: Create an action to update user information

--- src/server/actions/user.ts
+++ src/server/actions/user.ts
@@ -7,9 +7,11 @@ import prisma from "../db";
 import { z } from "zod";
 
 import { action } from "@/lib/action.client";
+import { userSchema } from "@/lib/validations/user";
 
 const imageSchema = z.string().url();
 
+//Check this action
 export const updateUserImage = action(imageSchema, async (image) => {
   const session = await auth();
 
@@ -20,3 +22,36 @@ export const updateUserImage = action(imageSchema, async (image) => {
 
   revalidatePath("/profile");
 });
+
+export const updateUser = action(
+  userSchema,
+  async ({ name, email, username, bio }) => {
+    const user = await prisma.user.findUnique({
+      where: { email },
+      select: { username: true },
+    });
+
+    if (user && user.username !== username) {
+      const userExistByUsername = await prisma.user.findUnique({
+        where: { username },
+        select: { username: true },
+      });
+
+      if (userExistByUsername) {
+        throw new Error("این نام کاربری قبلا ثبت شده است");
+      }
+    }
+
+    await prisma.user.update({
+      where: { email },
+      data: {
+        name,
+        email,
+        username,
+        bio,
+      },
+    });
+
+    revalidatePath("/profile");
+  },
+);
