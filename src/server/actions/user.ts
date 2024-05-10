e7eaadc4a8d9347499e59601cbe603a36246a4bf
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-05-10 22:56:45 +0330

    feat: Create an action to edit/set user password

--- src/server/actions/user.ts
+++ src/server/actions/user.ts
@@ -3,10 +3,12 @@
 import { revalidatePath } from "next/cache";
 
 import { auth } from "../auth";
+import { getUserByUsername } from "../data/user";
 import prisma from "../db";
+import { compare, hash } from "bcryptjs";
 import { z } from "zod";
 
-import { action } from "@/lib/action.client";
+import { action, authAction } from "@/lib/action.client";
 import { userSchema } from "@/lib/validations/user";
 
 const imageSchema = z.string().url();
@@ -51,6 +53,41 @@ export const updateUser = action(
         bio,
       },
     });
+
+    revalidatePath("/profile");
+  },
+);
+
+export const updateUserPassword = authAction(
+  z.object({ currentPassword: z.string(), newPassword: z.string() }),
+  async ({ currentPassword, newPassword }, { username }) => {
+    const user = await getUserByUsername(username);
+
+    if (!user) {
+      throw new Error("کاربر یافت نشد");
+    }
+
+    if (!user.password) {
+      const passwordHash = await hash(newPassword, 10);
+
+      await prisma.user.update({
+        where: { username },
+        data: { password: passwordHash },
+      });
+    } else {
+      const passwordMatch = await compare(currentPassword, user.password);
+
+      if (!passwordMatch) {
+        throw new Error("رمز عبور وارد شده اشتباه است");
+      }
+
+      const passwordHash = await hash(newPassword, 10);
+
+      await prisma.user.update({
+        where: { username },
+        data: { password: passwordHash },
+      });
+    }
 
     revalidatePath("/profile");
   },
