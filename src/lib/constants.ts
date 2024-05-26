ecc4d0a7b4939fdb2d0cf27b6eda8b636738f5df
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-05-26 22:18:09 +0330

    feat: Add tiptap extensions

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,13 @@
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import Highlight from "@tiptap/extension-highlight";
+import Link from "@tiptap/extension-link";
+import Placeholder from "@tiptap/extension-placeholder";
+import TextAlign from "@tiptap/extension-text-align";
+import Underline from "@tiptap/extension-underline";
+import type { Extensions } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { all, createLowlight } from "lowlight";
+
 import { NavItem } from "@/types";
 
 export const navLinks: NavItem[] = [
@@ -49,6 +59,29 @@ export const transliterationMap: Record<string, string> = {
   ه: "h",
   ی: "y",
 };
+
+export const editorExtensions: Extensions = [
+  StarterKit.configure({ heading: { levels: [1, 2, 3] } }),
+  TextAlign.configure({
+    defaultAlignment: "right",
+    types: ["heading", "paragraph"],
+  }),
+  Placeholder.configure({
+    placeholder: "متن مورد نظر خود را در اینجا بنویسید...",
+  }),
+  CodeBlockLowlight.configure({
+    lowlight: createLowlight(all),
+    HTMLAttributes: {
+      dir: "ltr",
+    },
+  }),
+  Link.configure({
+    openOnClick: "whenNotEditable",
+    validate: (href) => /^https?:\/\//.test(href),
+  }),
+  Highlight,
+  Underline,
+];
 
 export const DEFAULT_SERVER_ERROR =
   "خطایی در سمت سرور رخ داده است. لطفا مجددا تلاش کنید.";
