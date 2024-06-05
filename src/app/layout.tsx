324665b2cd2c6520a404dbdbf96a2b3ef8c75c7d
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-06-05 17:29:57 +0330

    feat: Remove session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import { cn } from "@/lib/utils";
 
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
-import SessionProvider from "@/components/providers/session-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import ToasterProvider from "@/components/providers/toaster-provider";
 
@@ -56,21 +55,19 @@ export default function RootLayout({
           vazirmatn.variable,
         )}
       >
-        <SessionProvider>
-          <ThemeProvider attribute="class" defaultTheme="light">
-            <div className="flex min-h-screen w-full flex-col">
-              <Navbar />
+        <ThemeProvider attribute="class" defaultTheme="light">
+          <div className="flex min-h-screen w-full flex-col">
+            <Navbar />
 
-              <main className="container grid flex-1 grid-cols-1 grid-rows-1">
-                {children}
-              </main>
+            <main className="container grid flex-1 grid-cols-1 grid-rows-1">
+              {children}
+            </main>
 
-              <Footer />
-            </div>
+            <Footer />
+          </div>
 
-            <ToasterProvider />
-          </ThemeProvider>
-        </SessionProvider>
+          <ToasterProvider />
+        </ThemeProvider>
       </body>
     </html>
   );
