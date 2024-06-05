635091220c23bef82041e9f518374011cda388e3
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-06-05 17:42:00 +0330

    feat: Improve post validations

--- src/lib/validations/post.ts
+++ src/lib/validations/post.ts
@@ -2,11 +2,14 @@ import { z } from "zod";
 
 export const postFormSchema = z.object({
   title: z
-    .string()
+    .string({ required_error: "عنوان مقاله الزامی می باشد" })
     .min(3, "عنوان حداقل باید 3 کاراکتر باشد")
     .max(100, "عنوان حداکثر باید 100 کاراکتر باشد"),
-  content: z.string(),
+  content: z
+    .string({ required_error: "محتوای مقاله الزامی می باشد" })
+    .min(1, "محتوای مقاله الزامی می باشد"),
   category: z.string({
-    required_error: "یک دسته بندی برای مقاله انتخاب کنید",
+    required_error: "دسته بندی مقاله الزامی می باشد",
   }),
+  image: z.instanceof(File, { message: "تصویر مقاله الزامی می باشد" }),
 });
