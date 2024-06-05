6c9e5211033bcd74774a5080e42bc0e002459118
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-06-05 18:00:30 +0330

    feat: Create a page for creating a new blog post

---
+++ src/app/create-post/page.tsx
@@ -0,0 +1,18 @@
+import { getAllCategories } from "@/server/data/post";
+
+import PostForm from "@/components/forms/post-form";
+
+export default async function CreatePostPage() {
+  const categories = await getAllCategories();
+
+  return (
+    <section className="flex flex-col gap-3">
+      <h1 className="flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0">
+        ایجاد مقاله جدید
+      </h1>
+      <div className="rounded-lg bg-primary-foreground p-4">
+        <PostForm type={"Create"} categories={categories} />
+      </div>
+    </section>
+  );
+}
