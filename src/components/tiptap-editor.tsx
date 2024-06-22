c2b8f6a03ba019dc0d908ef444736a14ec0474ed
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-06-22 08:00:21 +0330

    feat: Use text-foreground class instead of text-primary

--- src/components/tiptap-editor.tsx
+++ src/components/tiptap-editor.tsx
@@ -15,7 +15,7 @@ export default function TiptapEditor({ onChange }: Props) {
     editorProps: {
       attributes: {
         class:
-          "prose prose-sm min-h-[500px] w-full max-w-full rounded-md border border-input bg-background px-3 py-2 text-primary ring-offset-background dark:prose-invert sm:prose-base placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "prose prose-sm min-h-[500px] w-full max-w-full rounded-md border border-input bg-background px-3 py-2 text-foreground ring-offset-background dark:prose-invert sm:prose-base placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
       },
     },
     onUpdate({ editor }) {
