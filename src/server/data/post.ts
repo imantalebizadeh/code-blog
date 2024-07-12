97fd20c8ce4e768d712da51590e9c35b99cf1add
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-07-12 23:32:35 +0330

    feat: Write a function to get posts

--- src/server/data/post.ts
+++ src/server/data/post.ts
@@ -2,6 +2,25 @@ import { cache } from "react";
 
 import prisma from "../db";
 
+export const getPosts = cache(
+  async ({ limit, skip }: { limit?: number; skip?: number }) => {
+    try {
+      const posts = await prisma.post.findMany({
+        skip,
+        take: limit,
+        orderBy: { createdAt: "desc" },
+        include: { author: true, category: true },
+      });
+      return posts;
+    } catch (error) {
+      console.error(error);
+      throw new Error("خطای نامشخص, لطفا مجددا تلاش کنید");
+    }
+  },
+);
+
+export const getPostsCount = async () => await prisma.post.count();
+
 export const getPostById = cache(async (postId: string) => {
   try {
     const post = await prisma.post.findUnique({ where: { id: postId } });
