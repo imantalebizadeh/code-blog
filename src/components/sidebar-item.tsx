824d44841171bdf2354a2523eafa6eae55d50841
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-07-12 23:22:06 +0330

    feat: Remove the key assigned to the li tag

--- src/components/sidebar-item.tsx
+++ src/components/sidebar-item.tsx
@@ -5,14 +5,12 @@ import { cn } from "@/lib/utils";
 
 export default function SidebarItem({
   href,
-  key,
   children,
 }: React.ComponentPropsWithoutRef<typeof Link>) {
   const pathname = usePathname();
 
   return (
     <li
-      key={key}
       className={cn(
         "flex h-9 shrink-0 items-center justify-center px-3 py-2 font-semibold md:justify-start",
         {
