ca7c068471476d6d90478adcbf5c3ccaf6cf5d07
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-07-12 23:12:53 +0330

    feat: Show the list of posts in the root page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div>بدنه اصلی</div>;
+import { getPosts, getPostsCount } from "@/server/data/post";
+
+import PostList from "@/components/post-list";
+
+export default async function Home() {
+  const postsData = getPosts({ limit: 34 });
+  const postsCountData = getPostsCount();
+
+  const [posts, postsCount] = await Promise.all([postsData, postsCountData]);
+
+  return <PostList InitialPosts={posts} postsCount={postsCount} mode="home" />;
 }
