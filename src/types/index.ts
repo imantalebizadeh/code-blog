ebfaaf72bcc193551c0e2b37045faa584b0575c6
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-07-12 23:40:05 +0330

    feat: Create an alternative type for the post with its author and category

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,10 @@
+import { Category, Post, User } from "@prisma/client";
+
 export type NavItem = {
   title: string;
   href: string;
+};
+export type BlogPost = Post & {
+  author: User;
+  category: Category;
 };
