4b0898126738a0d0bff7e31d36261545af907d9a
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-07-15 19:12:24 +0330

    refactor: Use prisma itself instead of the function to get the number of posts

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,11 @@
-import { getPosts, getPostsCount } from "@/server/data/post";
+import { getPosts } from "@/server/data/post";
+import prisma from "@/server/db";
 
 import PostList from "@/components/post-list";
 
 export default async function Home() {
   const postsData = getPosts({ limit: 34 });
-  const postsCountData = getPostsCount();
+  const postsCountData = await prisma.post.count();
 
   const [posts, postsCount] = await Promise.all([postsData, postsCountData]);
 
