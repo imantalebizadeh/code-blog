52a81733a5ba72c97357f0af012d222154c6fbc9
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-07-31 17:30:51 +0330

    feat: Create a type for sub-comments

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,13 @@
-import { Category, Post, User } from "@prisma/client";
+import { Category, Comment, Post, User } from "@prisma/client";
 
 export type NavItem = {
   title: string;
   href: string;
 };
+
 export type BlogPost = Post & {
   author: User;
   category: Category;
 };
+
+export type SubComment = { id: string; authorName: string } | null;
