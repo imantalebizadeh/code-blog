8a44f2dd551377023b6a035aeaad5581ee1a7dc1
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-08-06 18:20:00 +0330

    refactor: Rewrite the PostList component from scratch to show only the list of articles

--- src/components/post-list.tsx
+++ src/components/post-list.tsx
@@ -1,68 +1,47 @@
-"use client";
+import type { InfiniteData } from "@tanstack/react-query";
 
-import { useEffect, useState } from "react";
-
-import { useIntersectionObserver } from "usehooks-ts";
-
-import { fetchPosts } from "@/server/actions/post";
-
-import { cn } from "@/lib/utils";
+import { cn, isBookmark } from "@/lib/utils";
 
 import PostItem from "./post-item";
-import type { BlogPost } from "@/types";
+import type {
+  ApiResponse,
+  BookmarkWithPost,
+  PostWithAuthorAndCategory,
+} from "@/types";
 
 type PostListProps = {
-  mode: "profile" | "home";
-  limit?: number;
-  InitialPosts: BlogPost[];
-  postsCount: number;
-  filter?: {
-    category?: string;
-  };
+  data: InfiniteData<
+    ApiResponse<PostWithAuthorAndCategory[] | BookmarkWithPost[]>
+  >;
+  viewMode?: "profile" | "home";
 };
 
-export default function PostList({
-  InitialPosts,
-  postsCount,
-  mode,
-  filter,
-}: PostListProps) {
-  const [posts, setPosts] = useState<BlogPost[]>(InitialPosts);
-  const [skip, setSkip] = useState(0);
-
-  const { isIntersecting, ref } = useIntersectionObserver({
-    threshold: 0.5,
-    onChange(isIntersecting) {
-      if (isIntersecting) {
-        setSkip(skip + 34);
-      }
-    },
-  });
-
-  useEffect(() => {
-    if (isIntersecting) {
-      fetchPosts({ limit: 34, skip, filter }).then((res) => {
-        setPosts([...posts, ...res]);
-      });
-    }
-  }, [skip]);
-
+export default function PostList({ data, viewMode = "home" }: PostListProps) {
   return (
-    <div className="flex flex-col items-center gap-10">
-      <section className="grid grid-cols-1 gap-x-10 gap-y-8 md:grid-cols-3">
-        {posts.map((post) => (
-          <PostItem key={post.id} post={post} mode={mode} />
-        ))}
-      </section>
-
-      <div
-        ref={ref}
-        className={cn("mx-auto block text-muted-foreground", {
-          hidden: !posts.length || posts.length === postsCount,
-        })}
-      >
-        در حال بارگذاری...
-      </div>
-    </div>
+    <>
+      {data.pages.map((page) => (
+        <section
+          key={page.currentPage}
+          className={cn("grid grid-cols-1 gap-x-10 gap-y-8 md:grid-cols-2", {
+            "lg:grid-cols-3": viewMode === "home",
+            "lg:grid-cols-4": viewMode === "profile",
+          })}
+        >
+          {page.data.map((data) => {
+            if (isBookmark(data)) {
+              return (
+                <PostItem
+                  key={data.post.id}
+                  post={data.post}
+                  viewMode={viewMode}
+                />
+              );
+            } else {
+              return <PostItem key={data.id} post={data} viewMode={viewMode} />;
+            }
+          })}
+        </section>
+      ))}
+    </>
   );
 }
