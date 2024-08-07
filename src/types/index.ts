b29cfbe2a95e818dce11637eae2fdabd0952988a
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-08-07 11:38:50 +0330

    feat: Create some types for api response and articles

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,36 @@
-import { Category, Comment, Post, User } from "@prisma/client";
+import { Prisma } from "@prisma/client";
+
+const postWithAuthorAndCategory = Prisma.validator<Prisma.PostDefaultArgs>()({
+  include: { author: true, category: true },
+});
+
+const bookmarkWithPost = Prisma.validator<Prisma.BookmarkDefaultArgs>()({
+  include: { post: { include: { author: true, category: true } } },
+});
+
+export type PostWithAuthorAndCategory = Prisma.PostGetPayload<
+  typeof postWithAuthorAndCategory
+>;
+
+export type BookmarkWithPost = Prisma.BookmarkGetPayload<
+  typeof bookmarkWithPost
+>;
+
+export type ApiResponse<T> = {
+  data: T;
+  currentPage: number;
+  nextPage: number | null;
+};
+
+export type fetcherOptions = {
+  endpoint: string;
+  searchParams?: Record<string, string>;
+  segments?: string[];
+};
 
 export type NavItem = {
   title: string;
   href: string;
-};
-
-export type BlogPost = Post & {
-  author: User;
-  category: Category;
 };
 
 export type SubComment = { id: string; authorName: string } | null;
