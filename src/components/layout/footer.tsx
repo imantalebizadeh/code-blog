253a030fdad739c5d2ccd8b7c8ea8d0c5a42489b
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-08-15 23:28:26 +0330

    feat: Put the required information in the footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,3 +1,27 @@
 export default function Footer() {
-  return <footer className="container py-4 text-center">ساخته شده توسط ایمان طابی زاده</footer>;
+  return (
+    <footer className="container mt-10 py-8">
+      <p className="text-center text-sm">
+        ساخته شده توسط{" "}
+        <a
+          href="https://github.com/imantalebizadeh"
+          className="font-medium underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ایمان طابی زاده
+        </a>
+        {" . "}
+        سورس کد پروژه در{" "}
+        <a
+          href="https://github.com/imantalebizadeh/code-blog"
+          className="font-medium underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          گیت هاب
+        </a>
+      </p>
+    </footer>
+  );
 }
