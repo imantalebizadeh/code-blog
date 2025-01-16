7070b1f820238984958657316ba785d47017f31a
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-01-16 18:19:10 +0330

    chore: Remove Prisma schema and migration files
    - Deleted the Prisma schema file and all associated migration files, including those for accounts, users, posts, comments, categories, bookmarks, and tags.
    - This cleanup removes obsolete database structures and prepares for a fresh schema design.

--- prisma/migrations/20240729144812_fix_comment_refrential_action/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `comments` DROP FOREIGN KEY `comments_parentId_fkey`;
-
--- AddForeignKey
-ALTER TABLE `comments` ADD CONSTRAINT `comments_parentId_fkey` FOREIGN KEY (`parentId`) REFERENCES `comments`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
