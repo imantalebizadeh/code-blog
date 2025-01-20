c416b109e06aea3106fc26e415a066344e63cc2a
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-01-20 19:53:17 +0330

    feat: Enhance environment variable management in env.js
    - Added client configuration for NEXT_PUBLIC_APP_URL with URL validation.
    - Updated experimental runtime environment to include NEXT_PUBLIC_APP_URL from process.env.
    - Introduced skipValidation option based on the SKIP_ENV_VALIDATION environment variable.

--- src/env.js
+++ src/env.js
@@ -12,7 +12,13 @@ export const env = createEnv({
     S3_ACCESS_KEY: z.string().min(1),
     S3_SECRET_KEY: z.string().min(1),
   },
-  experimental__runtimeEnv: process.env,
+  client: {
+    NEXT_PUBLIC_APP_URL: z.string().url(),
+  },
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  },
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
   isServer: typeof window === "undefined",
   emptyStringAsUndefined: true,
 });
